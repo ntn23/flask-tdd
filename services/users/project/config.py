e9634e532a1ecf718b8743d78ebe9653d35ba7fe
Nitin <chandranitin144@gmail.com>
2019-06-30 19:40:18 +0530

    Flask Debug Toolbar

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -8,19 +8,20 @@ class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
-    SECRET_KEY = 'my_precious' 
+    SECRET_KEY = 'my_precious'
+    DEBUG_TB_ENABLED = False              # new
+    DEBUG_TB_INTERCEPT_REDIRECTS = False  # new
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
-
+    DEBUG_TB_ENABLED = True
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
-
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
